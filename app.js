51539ea6fee180667a6db64e06d45b80854d01da
Pradeepkumar1407 <pradeephamray@gmail.com>
2025-12-05 07:18:03 +0000

    testing jenkins

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD Pipeline! building Docker images');
+  res.send('Hello from Jenkins CI/CD Pipeline! building Docker images and Container');
 });
 
 app.listen(3000, () => {
